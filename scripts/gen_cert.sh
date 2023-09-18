74deca76f66e7cfc71a5aa84f389a84f33b1560d
Kirill Kiselev <lirikk285@mail.ru>
2023-09-18 12:56:41 +0300

    update script

--- scripts/gen_cert.sh
+++ scripts/gen_cert.sh
@@ -1,13 +1,16 @@
-cat > cert.conf <<EOF
+#bin/sh
 
+DOMAIN="$1"
+SERIAL="$2"
+
+cat > cert.conf <<EOF
 authorityKeyIdentifier=keyid,issuer
 basicConstraints=CA:FALSE
-keyUsage = digitalSignature, nonRepudiation, keyEncipherment, dataEncipherment
-subjectAltName = @alt_names
+keyUsage=digitalSignature, nonRepudiation, keyEncipherment, dataEncipherment
+subjectAltName=@alt_names
 
 [alt_names]
-DNS.1 = $1
-
+DNS.1 = $DOMAIN
 EOF
 
 cat > csr.conf <<EOF
@@ -19,15 +22,16 @@ req_extensions = req_ext
 distinguished_name = dn
 
 [ dn ]
-CN = $1
+CN = $DOMAIN
 
 [ req_ext ]
 subjectAltName = @alt_names
 
 [ alt_names ]
-DNS.1 = $1
-
+DNS.1 = $DOMAIN
 EOF
 
-
 openssl req -new -key cert.key -sha256 -config csr.conf | openssl x509 -req -days 3650 -CA ca.crt -CAkey ca.key -set_serial "$2" -out /certs/"$1.crt" -extfile cert.conf 
+
+
+
